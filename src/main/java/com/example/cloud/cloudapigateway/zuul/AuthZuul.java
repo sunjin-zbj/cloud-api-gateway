3223dc1c80a4a40ce1dfe77460b284cc8e5eda08
sunjin991178@163.com <sunjin991178@163.com>
2020-06-17 17:43:26 +0800

    fix

--- src/main/java/com/example/cloud/cloudapigateway/zuul/AuthZuul.java
+++ src/main/java/com/example/cloud/cloudapigateway/zuul/AuthZuul.java
@@ -2,6 +2,9 @@ package com.example.cloud.cloudapigateway.zuul;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
 /**
  * @Description:
@@ -9,26 +12,33 @@ import com.netflix.zuul.exception.ZuulException;
  * @Mail: sunjin@sudiyi.cn
  * @Date: 2020/5/28
  */
+@Component
 public class AuthZuul extends ZuulFilter {
 
     @Override
     public String filterType() {
-        return "pro";
+        return PRE_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return 0;
+        return 1000;
     }
 
     @Override
     public boolean shouldFilter() {
+        System.out.println("zuulshouldFilter 校验");
         return true;
     }
 
     @Override
     public Object run() throws ZuulException {
         System.out.println("zuul 校验");
+        doSimeSing();
         return "zuul";
+    }
+
+    private void doSimeSing() {
+        throw new RuntimeException("zuul run exeception....");
     }
 }
